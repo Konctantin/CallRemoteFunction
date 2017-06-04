bfd185a994be8d4bed3be92d6ba5958ab03edff7
Konstantin <gawrilyako@gmail.com>
2017-06-04 09:08:54 +0300

    fix
    Signed-off-by: Konstantin <gawrilyako@gmail.com>

--- WowFake/WowFake.cpp
+++ WowFake/WowFake.cpp
@@ -2,128 +2,14 @@
 #include <tchar.h>
 #include <Windows.h>
 
-char locale[] = { 'r', 'u', 'R', 'U' };
-
-typedef struct {
-    void* vTable;
-    BYTE* buffer;
-    DWORD base;
-    DWORD alloc;
-    DWORD size;
-    DWORD read;
-} CDataStore;
-
 #define NOINLINE_C extern "C" __declspec(noinline)
 
-DWORD MainLoop();
-
-NOINLINE_C DWORD Test();
-NOINLINE_C DWORD Test2();
-NOINLINE_C DWORD NetClient__Send(CDataStore* packet);
-NOINLINE_C DWORD FrameScript_ExequteBuffer(char* src, char* path, DWORD state);
-NOINLINE_C DWORD FrameScript_ExequteBuffer2(char* src, char* path, DWORD state, DWORD lol1, DWORD lol2, DWORD lol3, DWORD64 lol4);
-NOINLINE_C DWORD __fastcall NetClient_Send2(void* thisPTR, void* dummy, CDataStore* ds, DWORD connectionId);
-NOINLINE_C DWORD __fastcall NetClient_ProcessMessage(void* thisPTR, void* dummy, void* param1, void* param2, CDataStore* ds, void* param4);
-
-int _tmain(int argc, _TCHAR* argv[])
+NOINLINE_C DWORD LoopBody()
 {
-    printf("CONTEXT = %i\n\n", sizeof(CONTEXT));
-
-    CONTEXT context;
-    context.ContextFlags = 0xAABBCC;
-    context.Rip = 0xDDFF110011;
-
-    void* ptr = &context;
-    void* cf  = &context.ContextFlags;
-    void* rip = &context.Rip;
-
-    printf("context: 0x%X, ContextFlags: 0x%X, Rip: 0x%X\n\n\n", ptr, cf, rip);
-    printf("context: 0x%X, ContextFlags: +0x%X, Rip: +0x%X\n\n\n", ptr, (DWORD64)cf - (DWORD64)ptr, (DWORD64)rip - (DWORD64)ptr);
-
-
-    FlushInstructionCache(0, 0, 0);
-
-    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&MainLoop, NULL, 0, NULL);
-
-    Test();
-
-    FrameScript_ExequteBuffer("print('test');", "lua.lua", 1);
-
-    FrameScript_ExequteBuffer2("src", "path", 0, 1, 2, 3, 4);
-
-    while (true);
-
-    return 0;
-}
-
-DWORD MainLoop()
-{
-    //printf("\nthread: %i\n", *(DWORD*)param);
-    CDataStore ds;
-    CDataStore ds2;
-
-    GetThreadContext(0, 0);
-
-    int i = 0;
-    while (1)
-    {
-        printf("%i\n", ++i);
-
-        int s = i > 10000 ? 1 : 0;
-
-        if (i > 10000000)
-        {
-            WOW64_CONTEXT ctx64;
-            CONTEXT ctx;
-            ctx.ContextFlags = 0x100001;
-            ctx.Rip = 0xFFAADD;
-            Test2();
-            locale[0] = 'x';
-            FrameScript_ExequteBuffer("print('test');", "lua.lua", i);
-
-            NetClient__Send(nullptr);
-            NetClient_Send2((void*)&ctx, (void*)56, &ds, s);
-
-            FrameScript_ExequteBuffer2("src", "path", 0, 1, 2, 3, 4);
-        }
-
-        ds.size = 44 + i;
-        NetClient_Send2((void*)5, (void*)56, &ds, s);
-
-        ds2.size = 155 + i;
-        NetClient_ProcessMessage((void*)12, (void*)66, (void*)100, (void*)200, &ds2, (void*)s);
-
-        for (int j = 0; j < 200; ++j)
-            Sleep(10);
-    }
-}
-
-NOINLINE_C DWORD Test()
-{
-    FrameScript_ExequteBuffer("print('test');", "lua.lua", 0);
-    return 0;
-}
-
-NOINLINE_C DWORD Test2()
-{
-    FrameScript_ExequteBuffer2("src", "path", 0, 1, 0xddaaffee, 3, 0xddaaffeeccccdd);
-    return 0;
-}
-
-NOINLINE_C DWORD NetClient__Send(CDataStore* packet)
-{
-    if (packet != nullptr)
-    {
-        WCHAR ch[0x1000] = { 0 };
-        auto code = GetLastError();
-        FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), ch, 0x1000, NULL);
-        printf("Error Code: %d\nError Name: %s", code, ch);
-
-        printf("DS ->: ", packet->size);
-        packet->size = 5000;
-        printf("DS <-: ", packet->size);
-    }
-    return 0;
+	int i = 5; // << inject jump
+	int b = 9;
+	printf(".");
+	return i + b;
 }
 
 NOINLINE_C DWORD FrameScript_ExequteBuffer(char* src, char* path, DWORD state)
@@ -135,31 +21,131 @@ NOINLINE_C DWORD FrameScript_ExequteBuffer(char* src, char* path, DWORD state)
     return 0;
 }
 
-NOINLINE_C DWORD FrameScript_ExequteBuffer2(char* src, char* path, DWORD state, DWORD lol1, DWORD lol2, DWORD lol3, DWORD64 lol4)
+NOINLINE_C DWORD Test()
 {
-    printf("\ncall FrameScript_ExequteBuffer2\n");
-    printf("Patn: %s\n", path);
-    printf("Src: %s\n", src);
-    printf("State: %i\n", state);
-
-    printf("lol1: %i\n", lol1);
-    printf("lol2: %i\n", lol2);
-    printf("lol3: %i\n", lol3);
-    printf("lol4: %i\n\n", lol4);
-
-    return 0;
+	FrameScript_ExequteBuffer("print('test');", "lua.lua", 0);
+	return 0;
 }
 
-NOINLINE_C DWORD __fastcall NetClient_Send2(void* thisPTR, void* dummy, CDataStore* ds, DWORD connectionId)
+NOINLINE_C DWORD Body()
 {
-    if (connectionId)
-        printf("CMSG Size: %i\n", ds->size);
-    return 0;
+	int arr[100] = { 0 }; // << inject executor
+
+	arr[0] = 1;
+	arr[1] = 2;
+	arr[2] = 3;
+	arr[3] = 4;
+	arr[4] = 5;
+	arr[5] = 6;
+	arr[6] = 7;
+	arr[7] = 8;
+	arr[8] = 9;
+	arr[9] = 10;
+	arr[10] = 11;
+	arr[11] = 12;
+	arr[12] = 13;
+	arr[13] = 14;
+	arr[14] = 15;
+	arr[15] = 16;
+	arr[16] = 17;
+	arr[17] = 18;
+	arr[18] = 19;
+	arr[19] = 20;
+	arr[20] = 21;
+	arr[21] = 22;
+	arr[22] = 23;
+	arr[23] = 24;
+	arr[24] = 25;
+	arr[25] = 26;
+	arr[26] = 27;
+	arr[27] = 28;
+	arr[28] = 29;
+	arr[29] = 30;
+	arr[30] = 31;
+	arr[31] = 32;
+	arr[32] = 33;
+	arr[33] = 34;
+	arr[34] = 35;
+	arr[35] = 36;
+	arr[36] = 37;
+	arr[37] = 38;
+	arr[38] = 39;
+	arr[39] = 40;
+	arr[40] = 41;
+	arr[41] = 42;
+	arr[42] = 43;
+	arr[43] = 44;
+	arr[44] = 45;
+	arr[45] = 46;
+	arr[46] = 47;
+	arr[47] = 48;
+	arr[48] = 49;
+	arr[49] = 50;
+	arr[50] = 51;
+	arr[51] = 52;
+	arr[52] = 53;
+	arr[53] = 54;
+	arr[54] = 55;
+	arr[55] = 56;
+	arr[56] = 57;
+	arr[57] = 58;
+	arr[58] = 59;
+	arr[59] = 60;
+	arr[60] = 61;
+	arr[61] = 62;
+	arr[62] = 63;
+	arr[63] = 64;
+	arr[64] = 65;
+	arr[65] = 66;
+	arr[66] = 67;
+	arr[67] = 68;
+	arr[68] = 69;
+	arr[69] = 70;
+	arr[70] = 71;
+	arr[71] = 72;
+	arr[72] = 73;
+	arr[73] = 74;
+	arr[74] = 75;
+	arr[75] = 76;
+	arr[76] = 77;
+	arr[77] = 78;
+	arr[78] = 79;
+	arr[79] = 80;
+	arr[80] = 81;
+	arr[81] = 82;
+	arr[82] = 83;
+	arr[83] = 84;
+	arr[84] = 85;
+	arr[85] = 86;
+	arr[86] = 87;
+	arr[87] = 88;
+	arr[88] = 89;
+	arr[89] = 90;
+	arr[90] = 91;
+	arr[91] = 92;
+	arr[92] = 93;
+	arr[93] = 94;
+	arr[94] = 95;
+	arr[95] = 96;
+	arr[96] = 97;
+	arr[97] = 98;
+	arr[98] = 99;
+	arr[99] = 100;
+
+	printf("\narr[100] = { 0 }\n");
+
+	return arr[98];
 }
 
-NOINLINE_C DWORD __fastcall NetClient_ProcessMessage(void* thisPTR, void* dummy, void* param1, void* param2, CDataStore* ds, void* param4)
+int _tmain(int argc, _TCHAR* argv[])
 {
-    if ((DWORD)param4)
-        printf("SMSG Size: %i Locale: %s\n", ds->size, locale);
-    return 0;
+	Body();
+	Test();
+
+	while (true)
+	{
+		LoopBody();
+		Sleep(100);
+	}
+	return 0;
 }
